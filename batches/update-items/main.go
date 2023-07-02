ede66882456a49f25a62846d8c88b8ac351c11cb
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-07-02 16:38:42 +0900

    print csv content

--- batches/update-items/main.go
+++ batches/update-items/main.go
@@ -5,37 +5,41 @@ import (
 	"fmt"
 	"net/http"
 
-	"golang.org/x/text/transform"
-
 	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 const (
-  API_URL = "https://www.city.bunkyo.lg.jp/library/opendata-bunkyo/01tetsuduki-kurashi/06bunbetuhinmoku/bunbetuhinmoku.csv"
+	API_URL = "https://www.city.bunkyo.lg.jp/library/opendata-bunkyo/01tetsuduki-kurashi/06bunbetuhinmoku/bunbetuhinmoku.csv"
 )
 
 func main() {
-  updateItemsFromCsv()
+	updateItemsFromCsv()
 }
 
 func updateItemsFromCsv() error {
-  resp, err := http.Get(
-    API_URL,
-  )
-  if err != nil {
-    return err
-  }
-  defer resp.Body.Close()
+	resp, err := http.Get(
+		API_URL,
+	)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-  r := csv.NewReader(transform.NewReader(resp.Body, japanese.ShiftJIS.NewDecoder()))
-  rows, err := r.ReadAll()
-  if err != nil {
-    return err
-  }
+	r := csv.NewReader(transform.NewReader(resp.Body, japanese.ShiftJIS.NewDecoder()))
+	rows, err := r.ReadAll()
+	if err != nil {
+		return err
+	}
 
-  for _, row := range rows {
-    fmt.Println(row)
-  }
+	for _, row := range rows {
+    item := row[1]
+    kind := row[2]
+    price := row[3]
+    remarks := row[4]
 
-  return nil
+    fmt.Println(item, kind, price, remarks)
+	}
+
+	return nil
 }
