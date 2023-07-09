f4f7042653d1100680228489f75b363d1fe36d0b
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-07-09 23:52:38 +0900

    それぞれの色を変更

--- frontend/components/Calendar.tsx
+++ frontend/components/Calendar.tsx
@@ -3,10 +3,24 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import { AreaCollectionDate } from "@/entity/area_collection_date";
 
 export default function Calendar({ areaCollectionDates }: { areaCollectionDates: AreaCollectionDate[] }) {
+  const backgroundColor = (kind: string) => {
+    switch (kind) {
+      case "可燃ごみ":
+        return "red";
+      case "不燃ごみ":
+        return "blue";
+      case "資源":
+        return "green";
+      default:
+        return "gray";
+    }
+  }
+
   const events = areaCollectionDates.map((areaCollectionDate: AreaCollectionDate) => {
     return {
       title: areaCollectionDate.kind,
-      start: areaCollectionDate.date
+      start: areaCollectionDate.date,
+      backgroundColor: backgroundColor(areaCollectionDate.kind)
     }
   });
 
