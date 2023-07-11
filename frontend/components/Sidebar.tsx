c063502c9fc4e9091a9ef3dce68c643136f14b50
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-07-11 19:15:43 +0900

    add kind

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -5,10 +5,24 @@ import Calendar from './Calendar'
 import React, { useEffect, useState } from 'react'
 import { AreaCollectionDate } from '../entity/area_collection_date'
 import { Item } from '../entity/item'
+import { Kind } from '../entity/kind'
 
 export default function Sidebar({ targetItem }: { targetItem: Item | null }) {
   const [areas, setAreas] = useState<Area[]>([])
   const [areaCollectionDates, setAreaCollectionDates] = useState<AreaCollectionDate[]>([])
+
+  const kindLabel = (kind: Kind) => {
+    switch (kind.name) {
+      case '可燃ごみ':
+        return <span className="ml-2 text-sm text-white bg-red-500 rounded px-2 py-1">{kind.name}</span>
+      case '不燃ごみ':
+        return <span className="ml-2 text-sm text-white bg-blue-600 rounded px-2 py-1">{kind.name}</span>
+      case '資源':
+        return <span className="ml-2 text-sm text-white bg-green-700 rounded px-2 py-1">{kind.name}</span>
+      case '粗大ごみ':
+        return <span className="ml-2 text-sm text-white bg-amber-800 rounded px-2 py-1">{kind.name}</span>
+    }
+  }
 
   const fetchAreas = async () => {
     const response = await fetch('http://127.0.0.1:3000/areas')
@@ -32,25 +46,37 @@ export default function Sidebar({ targetItem }: { targetItem: Item | null }) {
   }, [])
 
   return (
-    <div className="border-l-2 w-4/12">
-      <div className="flex justify-center mt-6">
-        <select onChange={handleChange} className="p-4 border-2 rounded-lg text-gray-600">
-          <option>エリアを選択してください</option>
-          {
-            areas.map((area) => (
-              <option key={area.id} value={area.id}>{area.name}</option>
-            ))
-          }
-        </select>
-     </div>
+    <>
+      <div className="border-l-2 w-4/12">
+        <div className="flex justify-center mt-6">
+          <select onChange={handleChange} className="p-4 border-2 rounded-lg text-gray-600">
+            <option>エリアを選択してください</option>
+            {
+              areas.map((area) => (
+                <option key={area.id} value={area.id}>{area.name}</option>
+              ))
+            }
+          </select>
+       </div>
 
-      <div className="m-4">
-        <Calendar areaCollectionDates={areaCollectionDates} />
-      </div>
+        <div className="m-4">
+          <Calendar areaCollectionDates={areaCollectionDates} />
+        </div>
 
-      <div className="flex justify-center mt-6">
-        {targetItem?.name}
+        {
+          targetItem &&
+          <div className="m-4">
+            <div className="flex items-center justify-center p-4 border-2 rounded-lg shadow-lg shadow-black-500/40">
+              <p className="text-md font-bold">{targetItem.name}</p>
+              {
+                targetItem.kinds.map((kind: Kind) => (
+                  kindLabel(kind)
+                ))
+              }
+            </div>
+          </div>
+        }
       </div>
-    </div>
+    </>
   )
 }
