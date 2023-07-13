d08fa89648a3f5d2ca9bd06c3fe12fce59575a28
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-07-13 19:54:27 +0900

    fix dc

--- backend/cmd/api/items/main_test.go
+++ backend/cmd/api/items/main_test.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/yoshiyoshiharu/item-throw-ways/model/entity"
+	"github.com/yoshiyoshiharu/item-throw-ways/model/repository"
 )
 
 func TestHandler(t *testing.T) {
-  fmt.Println("test")  
+  fmt.Println("test")
+
+  var kinds []entity.Kind
+
+  repository.Db.Find(&kinds)
+  fmt.Println(kinds)
 }
