0bba386d88ba132591da739bf2badff42bf51957
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-07-17 23:35:07 +0900

    add items test

--- backend/model/repository/item_test.go
+++ backend/model/repository/item_test.go
@@ -36,28 +36,51 @@ func TestItemRepository_DeleteAndAll(t *testing.T) {
 
   repo := NewItemRepository(gormDB)
 
-  item1 := &entity.Item{ID: 1, Name: "Item 1"}
-  item2 := &entity.Item{ID: 2, Name: "Item 2"}
-
-	items := []*entity.Item{
-    item1,
-    item2,
-	}
-
-	mock.ExpectBegin()
-	mock.ExpectExec("DELETE FROM items").WillReturnResult(sqlmock.NewResult(0, 0))
-	mock.ExpectExec("DELETE FROM item_kinds").WillReturnResult(sqlmock.NewResult(0, 0))
-	mock.ExpectExec(
-    regexp.QuoteMeta("INSERT INTO `items` (`name`,`name_kana`,`price`,`remarks`,`id`) VALUES (?,?,?,?,?),(?,?,?,?,?)")).
-    WithArgs(item1.Name, item1.NameKana, item1.Price, item1.Remarks, item1.ID, item2.Name, item2.NameKana, item2.Price, item2.Remarks, item2.ID).
-    WillReturnResult(sqlmock.NewResult(0, 2))
-	mock.ExpectCommit()
-
-  err := repo.DeleteAndInsertAll(items)
-
   t.Run("[正常系] itemsとitem_kindsを全消去し、itemsを挿入すること", func(t *testing.T) {
+    item1 := &entity.Item{ID: 1, Name: "Item 1"}
+    item2 := &entity.Item{ID: 2, Name: "Item 2"}
+
+    items := []*entity.Item{
+      item1,
+      item2,
+    }
+
+    mock.ExpectBegin()
+    mock.ExpectExec("DELETE FROM items").WillReturnResult(sqlmock.NewResult(0, 0))
+    mock.ExpectExec("DELETE FROM item_kinds").WillReturnResult(sqlmock.NewResult(0, 0))
+    mock.ExpectExec(
+      regexp.QuoteMeta("INSERT INTO `items` (`name`,`name_kana`,`price`,`remarks`,`id`) VALUES (?,?,?,?,?),(?,?,?,?,?)")).
+      WithArgs(item1.Name, item1.NameKana, item1.Price, item1.Remarks, item1.ID, item2.Name, item2.NameKana, item2.Price, item2.Remarks, item2.ID).
+      WillReturnResult(sqlmock.NewResult(0, 2))
+    mock.ExpectCommit()
+
+    err := repo.DeleteAndInsertAll(items)
+
     assert.NoError(t, err)
     assert.NoError(t, mock.ExpectationsWereMet())
-  })  
-}
+  })
 
+  t.Run("[異常系] itemsに不正な値があるときはRollbackし、エラーを返すこと", func(t *testing.T) {
+    item1 := &entity.Item{ID: 1, Name: "Item 1"}
+    invalidItem := &entity.Item{ID: 1, Name: "Item 2"}
+
+    items := []*entity.Item{
+      item1,
+      invalidItem,
+    }
+
+    mock.ExpectBegin()
+    mock.ExpectExec("DELETE FROM items").WillReturnResult(sqlmock.NewResult(0, 0))
+    mock.ExpectExec("DELETE FROM item_kinds").WillReturnResult(sqlmock.NewResult(0, 0))
+    mock.ExpectExec(
+      regexp.QuoteMeta("INSERT INTO `items` (`name`,`name_kana`,`price`,`remarks`,`id`) VALUES (?,?,?,?,?),(?,?,?,?,?)")).
+      WithArgs(item1.Name, item1.NameKana, item1.Price, item1.Remarks, item1.ID, invalidItem.Name, invalidItem.NameKana, invalidItem.Price, invalidItem.Remarks, invalidItem.ID).
+      WillReturnResult(sqlmock.NewResult(0, 2))
+    mock.ExpectRollback()
+
+    err := repo.DeleteAndInsertAll(items)
+
+    assert.Error(t, err)
+    assert.Error(t, mock.ExpectationsWereMet())
+  })
+}
