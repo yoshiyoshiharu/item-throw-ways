d99a026f4e75ce716f98511c2bf9f977469f7294
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-07-17 22:51:10 +0900

    area repoのテスト

--- backend/model/repository/area_test.go
+++ backend/model/repository/area_test.go
@@ -8,14 +8,20 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
-func TestFindAll(t *testing.T) {
+
+func newMockDB() (*gorm.DB, sqlmock.Sqlmock) {
   db, mock, _ := sqlmock.New()
-  defer db.Close()
 
   gormDB, _ := gorm.Open(mysql.New(mysql.Config{
 		Conn: db,
     SkipInitializeWithVersion: true,
 	}), &gorm.Config{})
+  
+  return gormDB, mock
+}
+
+func TestFindAll(t *testing.T) {
+  gormDB, mock := newMockDB()
 
   repo := NewAreaRepository(gormDB)
 
@@ -27,9 +33,31 @@ func TestFindAll(t *testing.T) {
 
 	areas := repo.FindAll()
 
-	assert.Equal(t, 2, len(areas))
-	assert.Equal(t, "テストエリア1", areas[0].Name)
-	assert.Equal(t, "テストエリア2", areas[1].Name)
+  t.Run("[正常系] FindAllは全件返すこと", func(t *testing.T) {
+    assert.Equal(t, 2, len(areas))
+    assert.Equal(t, "テストエリア1", areas[0].Name)
+    assert.Equal(t, "テストエリア2", areas[1].Name)
 
-	assert.NoError(t, mock.ExpectationsWereMet())
+    assert.NoError(t, mock.ExpectationsWereMet())
+  })
+}
+
+
+func TestFindById(t *testing.T) {
+  gormDB, mock := newMockDB()
+
+  repo := NewAreaRepository(gormDB)
+
+	rows := sqlmock.NewRows([]string{"id", "name"}).
+		AddRow(1, "テストエリア1")
+
+	mock.ExpectQuery("SELECT \\* FROM `areas` WHERE id = ?").WithArgs(1).WillReturnRows(rows)
+
+	area, _ := repo.FindById(1)
+
+  t.Run("[正常系] FindByIdは指定したIDのエリアを返すこと", func(t *testing.T) {
+    assert.Equal(t, "テストエリア1", area.Name)
+
+    assert.NoError(t, mock.ExpectationsWereMet())
+  })
 }
