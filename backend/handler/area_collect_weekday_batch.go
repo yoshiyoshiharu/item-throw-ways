b5a11a3fda0478ccb234596dbccc798147f7a55a
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-07-22 10:28:19 +0900

    area collelct weekday バッチにスラック通知を実装

--- backend/handler/area_collect_weekday_batch.go
+++ backend/handler/area_collect_weekday_batch.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/yoshiyoshiharu/item-throw-ways/domain/service"
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/yoshiyoshiharu/item-throw-ways/domain/service"
+)
 type AreaCollectWeekdayBatchHandler interface {
   UpdateAll()
 }
@@ -16,5 +21,36 @@ func NewAreaCollectWeekdayBatchHandler(service service.AreaCollectWeekdayBatchSe
 }
 
 func (h *areaCollectWeekdayBatchHandler) UpdateAll () {
-  h.s.UpdateAll()
+  err := h.s.UpdateAll()
+
+  notifySlack(err)
 }
+
+func notifySlack(err error) {
+  var jsonStr string
+
+  if err == nil {
+    jsonStr = `{"text":"` + "バッチ処理に成功しました。"  + `"}`
+  } else {
+    jsonStr = `{"text":"` + "バッチ処理でエラーが発生しました。: " + err.Error() + `"}`
+  }
+
+  req, err := http.NewRequest(
+    "POST",
+    "https://hooks.slack.com/services/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",
+    bytes.NewBuffer([]byte(jsonStr)),
+  )
+
+  if err != nil {
+    panic(err)
+  }
+
+  req.Header.Set("Content-Type", "application/json")
+
+  client := new(http.Client)
+  _, err = client.Do(req)
+
+  if err != nil {
+    panic(err)
+  }
+} 
